8e1ca2747ac7e34d902ba6d19e65d7d9f229bf38
Steven-Posterick <62454596+Steven-Posterick@users.noreply.github.com>
2021-12-13 00:16:23 -0600

    Rest of the Record logic

---
+++ Utils/Converters/StringToInt.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Inventory_Management.Utils.Converters
+{
+    public class StringToInt : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int intValue)
+                return intValue.ToString();
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var sValue = value?.ToString();
+            return int.TryParse(sValue, out var parsedInt) ? parsedInt : 0;
+        }
+    }
+}
