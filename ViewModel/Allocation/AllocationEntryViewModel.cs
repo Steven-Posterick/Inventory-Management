1023535f8f092f5a5b8688a375b6d610906de43b
Steven-Posterick <62454596+Steven-Posterick@users.noreply.github.com>
2021-12-13 22:12:21 -0600

    Revert "Merge pull request #2 from Steven-Posterick/megans-branch"
    This reverts commit 7a5e1e566eeab6df42f39c45cc2e48683974332a, reversing
    changes made to ad73c24a379cc36c3c5734e297d3a05b1cb88458.

--- ViewModel/Allocation/AllocationEntryViewModel.cs
+++ ViewModel/Allocation/AllocationEntryViewModel.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
-using System.Windows.Input;
-using Inventory_Management.Utils.Command;
 using Inventory_Management.Utils.Events.Allocation;
-using Inventory_Management.Utils.Events.Record;
-using Inventory_Management.View.Record;
 using Prism.Events;
 
 namespace Inventory_Management.ViewModel.Allocation
@@ -13,50 +9,17 @@ namespace Inventory_Management.ViewModel.Allocation
     
     public class AllocationEntryViewModel : BaseViewModel, IAllocationEntryViewModel
     {
-        private int _receiptId;
-        private int _receivedId;
-
-        public int ReceiptId
-        {
-            get => _receiptId;
-            set => SetProperty(ref _receiptId, value);
-        }
-
-        public int ReceivedId
-        {
-            get => _receivedId;
-            set => SetProperty(ref _receivedId, value);
-        }
-
-        private void OpenAllocation(int receiptId, int receivedId)
-        {
-            ReceiptId = receiptId;
-            ReceivedId = receivedId;
-        }
-
-        private readonly IEventAggregator _eventAggregator;
-
         public AllocationEntryViewModel(IServiceProvider serviceProvider, IEventAggregator eventAggregator) : base(serviceProvider)
         {
-            _eventAggregator = eventAggregator;
-            
             eventAggregator.GetEvent<OpenAllocationEvent>().Subscribe(x =>
             {
                 OpenAllocation(x.ReceiptId, x.ReceivedId);
             });
         }
 
-        // OpenReceived
-        public ICommand OpenReceived => CommandHelper.CreateCommand(() =>
+        private void OpenAllocation(int receiptId, int receivedId)
         {
-            OpenOrActivate<RecordEntry>(() =>
-            {
-                _eventAggregator.GetEvent<OpenRecordEvent>().Publish(ReceivedId);
-                //_eventAggregator.GetEvent<OpenRecordEvent>().Publish(ReceiptId);
-            });
-        });
-
-        // OpenReceipt
-
+            
+        }
     }
 }
